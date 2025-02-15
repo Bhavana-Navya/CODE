4ae3111bc6232a6e774d3e546621212ac5948fe6
Bhavana-Navya <24B11EC037@adityauniversity.in>
2025-02-15 11:24:37 +0530

    Add files via upload

---
+++ Quick_sort.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int partision(int a[],int key,int start,int end)
+{
+	int s=start;
+	int e=end;
+	while(s<e)
+	{
+		while(a[s]<=key)
+		{
+			s++;
+		}
+		while(a[e]>key)
+		{
+			e--;
+		}
+		//exchange
+		if(s<e)
+		{
+			int temp=a[s];
+			a[s]=a[e];
+			a[e]=temp;
+		}
+	}
+	int temp=a[e];
+	a[e]=key;
+	a[start]=temp;
+	return e;
+}
+void quicksort(int a[],int n,int start,int end)
+{
+	if(start>end)
+	return;
+	if(start<end)
+	{
+		int p=a[start];
+		p=partision(a,p,start,end);
+		//0 to pi-1 left part
+		quicksort(a,n,start,p-1);
+		//pi+1 to end
+		quicksort(a,n,p+1,end);
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int i,a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	quicksort(a,n,0,n-1);
+		for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
---
+++ merge_sort.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+//Merge sort
+void merge(int a[],int start,int mid,int end,int n)
+{
+	int i=start;//first part begin
+	int j=mid+1;//second part begin
+	int b[end-start+1];
+	int k=0;
+	while(i<=mid && j<=end)
+	{
+		if(a[i]<a[j])
+		{
+			b[k++]=a[i++];
+		}
+		else
+		{
+			b[k++]=a[j++];
+		}
+	}
+	while(i<=mid)
+		b[k++]=a[i++];
+	while(j<=end)
+		b[k++]=a[j++];
+	k=0;
+	for(i=start;i<=end;i++)
+	{
+		a[i]=b[k++];
+	}
+}
+void mergesort(int a[],int n,int start,int end)
+{
+	if(start>=end)
+	{
+		return;
+	}
+	int mid=(start+end)/2;
+	mergesort(a,n,start,mid);
+	mergesort(a,n,mid+1,end);
+	merge(a,start,mid,end,n);
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	mergesort(a,n,0,n-1);
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
